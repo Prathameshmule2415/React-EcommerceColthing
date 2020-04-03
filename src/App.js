7579277b400c23c9c6aba051a8cb3fcafe330e8f
Prathameshmule2415 <59178753+Prathameshmule2415@users.noreply.github.com>
2020-04-03 16:52:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Homepage from "./homepage.component";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <Homepage />
     </div>
   );
 }
